a1b886417dc12acf712a3f11f1b7b3193504ab69
wifibaby4u <ethanote@gmail.com>
2024-01-19 17:44:15 +0800

    upgrade version to 2.5.7

--- build-docker.sh
+++ build-docker.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-VERSION=2.5.5
+VERSION=2.5.7
 
 docker build --build-arg VERSION=$VERSION -t junit/hbase-base:$VERSION hbase-base
 docker build --build-arg VERSION=$VERSION -t junit/hbase-master:$VERSION hbase-master
